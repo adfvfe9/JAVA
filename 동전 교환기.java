4ea48ee6883e3885e2a1bffd27b47971109eb085
adfvfe9 <adfvfe9@gmail.com>
2025-04-16 18:59:41 +0900

    배열을 이용해서 코드를 더 단순화했고, 필요 없는 단위는 나오지 않게 변경함

--- 동전 교환기.java
+++ 동전 교환기.java
@@ -7,79 +7,18 @@ class moneyChanger {
         System.out.print("돈을 입력하세요 : ");
         int money = scan.nextInt();
         int moneys = 0;
+        int[] danwi = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        String[] won = {"장", "장", "장", "장", "개", "개", "개", "개", "원"};
 
-        if (money >= 50000) {
-            for (int unused = 0; money >= 50000; unused++) {
-                money -= 50000;
-                moneys++;
-            }
-            System.out.println("50000원 : " + moneys + "장");
-        }
-        if (money >= 10000) {
+        for (int i = 0; money > 0; i++) {
             moneys = 0;
-            for (int unused = 0; money >= 10000; unused++) {
-                money -= 10000;
+            for (int j = 0; money >= danwi[i]; j++) {
+                money -= danwi[i];
                 moneys++;
             }
-            System.out.println("10000원 : " + moneys + "장");
-        }
-        if (money >= 5000) {
-            moneys = 0;
-            for (int unused = 0; money >= 5000; unused++) {
-                money -= 5000;
-                moneys++;
+            if (moneys != 0) {
+                System.out.println(danwi[i] + "원 : " + moneys + won[i]);
             }
-            System.out.println("5000원 : " + moneys + "장");
-        }
-        if (money >= 1000) {
-            moneys = 0;
-            for (int unused = 0; money >= 1000; unused++) {
-                money -= 1000;
-                moneys++;
-            }
-            System.out.println("1000원 : " + moneys + "장");
-        }
-        if (money >= 500) {
-            moneys = 0;
-            for (int unused = 0; money >= 500; unused++) {
-                money -= 500;
-                moneys++;
-            }
-            System.out.println("500원 : " + moneys + "개");
-        }
-        if (money >= 100) {
-            moneys = 0;
-            for (int unused = 0; money >= 100; unused++) {
-                money -= 100;
-                moneys++;
-            }
-            System.out.println("100원 : " + moneys + "개");
-        }
-        if (money >= 50) {
-            moneys = 0;
-            for (int unused = 0; money >= 50; unused++) {
-                money -= 50;
-                moneys++;
-            }
-            System.out.println("50원 : " + moneys + "개");
-        }
-        if (money >= 10) {
-            moneys = 0;
-            for (int unused = 0; money >= 10; unused++) {
-                money -= 10;
-                moneys++;
-            }
-            System.out.println("10원 : " + moneys + "개");
-        }
-        if (money > 0) {
-            moneys = 0;
-            for (int unused = 0; money > 0; unused++) {
-                money -= 1;
-                moneys++;
-            }
-            System.out.println("남는 돈 : " + moneys + "원");
-        } else {
-            return;
         }
     }
 }
